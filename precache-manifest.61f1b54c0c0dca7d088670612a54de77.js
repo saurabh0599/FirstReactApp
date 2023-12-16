9b5327ff9cc98c2427aa3d79c7fcb57b62773047
Saurabh Gupta <saurabhg0599@gmai.com>
2023-12-16 10:14:16 +0530

    Updates

---
+++ precache-manifest.61f1b54c0c0dca7d088670612a54de77.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69fde653add1b94f0ed1277289c7c1c6",
+    "url": "/FirstReactApp/index.html"
+  },
+  {
+    "revision": "c8caf0bed047c5d5de7f",
+    "url": "/FirstReactApp/static/css/main.01cd87da.chunk.css"
+  },
+  {
+    "revision": "cd8314c965104b2d21b3",
+    "url": "/FirstReactApp/static/js/2.ecb774d4.chunk.js"
+  },
+  {
+    "revision": "c87ae3f2a437a1b2df8d",
+    "url": "/FirstReactApp/static/js/3.177faaa1.chunk.js"
+  },
+  {
+    "revision": "c8caf0bed047c5d5de7f",
+    "url": "/FirstReactApp/static/js/main.01b0cc81.chunk.js"
+  },
+  {
+    "revision": "44055a89dd255266307c",
+    "url": "/FirstReactApp/static/js/runtime~main.86504104.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/FirstReactApp/static/media/logo.06e73328.svg"
+  }
+]);
